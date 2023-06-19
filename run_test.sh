e9555d0df98e3a74d4df98ca3e3b8c2fd1175697
Lakshmi vara prasad Bommalata <46681044+prasad-blv@users.noreply.github.com>
2023-06-19 11:55:02 +0530

    run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -4,12 +4,12 @@ echo "GBCNET"
 python /content/GBCNET/test.py \
 	--test_set_name="test.txt" \
 	--patch=0.15 \
-	--load_path="/content/drive/MyDrive/GBCU/gbcnet.pth"
+	--load_path="/content/drive/MyDrive/GBCU/outputs/gbcnet.pth"
 
 echo "---------------------------"
 
-echo "GBCNET+Curriculum"
+echo "AEH+GBCNET"
 python /content/GBCNET/test.py \
 	--test_set_name="test.txt" \
 	--patch=0.17 \
-	--load_path="/content/drive/MyDrive/GBCU/gbcnet_va.pth"
+	--load_path="/content/drive/MyDrive/GBCU/outputs/gbcnet_va.pth"
